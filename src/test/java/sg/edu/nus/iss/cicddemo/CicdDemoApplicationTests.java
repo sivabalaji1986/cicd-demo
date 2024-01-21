d840b16012a11f7a83ca27c690edb9345bf7364b
sivabalaji1986 <42265473+sivabalaji1986@users.noreply.github.com>
2024-01-21 23:42:12 +0800

    Code Smell fix - 1
    Added assert for Context loading to fix code smell

--- src/test/java/sg/edu/nus/iss/cicddemo/CicdDemoApplicationTests.java
+++ src/test/java/sg/edu/nus/iss/cicddemo/CicdDemoApplicationTests.java
@@ -1,13 +1,16 @@
 package sg.edu.nus.iss.cicddemo;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class CicdDemoApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads(ApplicationContext context) {
+		assertThat(context).isNotNull();
 	}
 
 }
